99d4e1e168f8c05bf1ffaa393c51fad90ce6961d
LostCore <lostcore@gmail.com>
2016-09-23 12:10:55 +0200

    Ora se il prezzo della variazione è vuoto, viene preso il prezzo del prodotto parent

--- src/Plugin.php
+++ src/Plugin.php
@@ -24,6 +24,7 @@ class Plugin extends BasePlugin {
 
 		$this->define_public_hooks();
 		$this->define_admin_hooks();
+		$this->define_general_hooks();
 	}
 
 	/**
@@ -44,6 +45,43 @@ class Plugin extends BasePlugin {
 		$this->loader->add_action("woocommerce_variation_options_pricing",$plugin_admin,"inject_js_after_variation_prices", 10, 3);
 
 		$this->loader->add_action('woocommerce_process_product_meta_'.'variable', $plugin_admin, "save_prices_for_variable_products", 10, 1);
+	}
+
+	private function define_general_hooks(){
+		$this->loader->add_filter("get_"."post"."_metadata", $this, "get_parent_price_on_variation_price_get", 10, 4);
+	}
+
+	/**
+	 * WooCommerce use __get() method to retrieve $variation->price. The method search price meta on variation.
+	 * This method will override the value if
+	 *
+	 * @param $value
+	 * @param $object_id
+	 * @param $meta_key
+	 * @param $single
+	 *
+	 * @return string|array
+	 */
+	public function get_parent_price_on_variation_price_get($value, $object_id, $meta_key, $single){
+		if($meta_key == "_price" || $meta_key == "_sale_price" || $meta_key == "regular_price"){
+			if(get_post_type($object_id) == "product_variation"){
+				remove_filter( current_filter(), [$this,'get_parent_price_on_variation_price_get'] ); //Avoid infinite loop
+				//Try to get the price:
+				$variation_price = get_post_meta($object_id,$meta_key,true);
+				if($variation_price == ""){
+					$parent_price = call_user_func(function() use($object_id,$meta_key){
+						$v = wc_get_product($object_id);
+						$value = get_post_meta($v->parent->id,$meta_key,true);
+						return $value;
+					});
+					if($parent_price != ""){
+						$value = [$parent_price];
+					}
+				}
+				add_filter("get_"."post"."_metadata", [$this, "get_parent_price_on_variation_price_get"], 10, 4); //Re-add the filter
+			}
+		}
+		return $value;
 	}
 
 	/**
